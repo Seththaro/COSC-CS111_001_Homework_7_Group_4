b787c7bf9a97f2187574523b8df3c6cff2148a16
Seththaro <116055231+Seththaro@users.noreply.github.com>
2022-10-23 17:11:18 +0700

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -13,8 +13,8 @@ def remove_all_after(num_list, num_input):
         del num_list[num_input_indix+1:]
         return num_list
 
+    
 
-
-print(remove_all_after([1, 1, 2, 4, 5], 2))
-
-
+if __name__ == '__main__':
+    print(remove_all_after([1, 2, 3, 4, 5], 3))
+    print(remove_all_after([1, 1, 2, 2, 3, 3], 2))
