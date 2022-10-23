19122ad317ba483487faa4595ef427745657848a
MOKMonyratanak <116064521+MOKMonyratanak@users.noreply.github.com>
2022-10-23 17:06:23 +0700

    Create exercise5.py

---
+++ exercise5.py
@@ -0,0 +1,24 @@
+# In a given string you should reverse every word, but the words should stay in their places.
+
+def backward_string_by_word(text):        
+    return ' '.join(word[::-1] for word in text.split(' '))
+
+def init():
+    cont = True
+    while cont == True:
+        print("Welcome to the Text Reverser app!")
+        text = input("Please enter your word: ")
+        print(f'Your reversed text is "{backward_string_by_word(text)}"')
+        # Condition to loop the program
+        while True:
+            ans = input("Do you want to restart the application?(Yes/No): ")
+            if ans.lower() == 'yes':
+                cont = True
+                break
+            elif ans.lower() == 'no':
+                cont = False
+                break
+            else: 
+                print("Invalid Input")
+if __name__ == '__main__':
+  init()
