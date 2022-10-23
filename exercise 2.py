1dde3790503c1c3919466927902998a8f6fbf5e3
Seththaro <116055231+Seththaro@users.noreply.github.com>
2022-10-23 19:41:11 +0700

    Update exercise 2.py

--- exercise 2.py
+++ exercise 2.py
@@ -3,7 +3,11 @@ def index_power(array, n):
         return -1
     else:    
         return array[n]**n
-print(index_power([1, 2, 3, 4], 2))
-print(index_power([1, 3, 10, 100], 3))
-print(index_power([0, 1], 0))
-print(index_power([1, 2], 3))
+
+
+        
+if __name__ == '__main__':
+    print(index_power([1, 2, 3, 4], 2))
+    print(index_power([1, 3, 10, 100], 3))
+    print(index_power([0, 1], 0))
+    print(index_power([1, 2], 3))
